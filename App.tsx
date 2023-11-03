2fe440136e2eb626d27a5977befb4ba86b17f8b7
Amal <amalscreations04@gmail.com>
2023-11-03 13:46:09 +0530

    voice recognition module integrated

--- App.tsx
+++ App.tsx
@@ -1,95 +1,94 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import React from 'react';
-import type {PropsWithChildren} from 'react';
+import React, {useEffect, useState} from 'react';
+import Voice, {SpeechErrorEvent} from '@react-native-voice/voice';
 import {
   SafeAreaView,
   ScrollView,
-  StatusBar,
   StyleSheet,
   Text,
-  useColorScheme,
+  TouchableOpacity,
+  Image,
   View,
+  Dimensions,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
+const deviceHeight = Dimensions.get('window').height;
+const start = require('./assets/start.png');
+const stop = require('./assets/stop.png');
 
 function App(): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+  const [isRecording, setIsRecording] = useState(false);
+  const [result, setResult] = useState([]);
+  const [isStartRecording, setIsStartRecording] = useState(false);
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  useEffect(() => {
+    Voice.onSpeechStart = onSpeechStart;
+    Voice.onSpeechEnd = onSpeechEnd;
+    Voice.onSpeechResults = onSpeechResults;
+    Voice.onSpeechError = onSpeechError;
+
+    return () => {
+      Voice.destroy().then(Voice.removeAllListeners);
+    };
+  }, []);
+
+  const onSpeechStart = (e: any) => {
+    setIsStartRecording(true);
+  };
+
+  const onSpeechResults = (e: any) => {
+    setResult(e.value);
+    setIsStartRecording(false);
+  };
+
+  const onSpeechEnd = (e: any) => {
+    setIsRecording(false);
+  };
+
+  const onSpeechError = (e: SpeechErrorEvent) => {
+    onSpeechResults;
+  };
+
+  const startVoiceRecognizing = async () => {
+    try {
+      await Voice.start('en-US');
+
+      setResult([]);
+      setIsRecording(true);
+    } catch (error) {}
+  };
+
+  const stopVoiceRecognizing = async () => {
+    try {
+      setIsRecording(false);
+
+      await Voice.stop();
+    } catch (error) {}
   };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
+    <SafeAreaView>
       <ScrollView
         contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+        style={styles.scrollView}>
+        <Text style={styles.appHeading}>Voice to Text</Text>
+        <View style={styles.textView}>
+          {isStartRecording && (
+            <Text style={styles.recording}>Recording.....</Text>
+          )}
+          {result.map((text: string) => (
+            <Text style={styles.searchResult}>{text}</Text>
+          ))}
+        </View>
+        <View style={styles.actionButtonView}>
+          {!isRecording ? (
+            <TouchableOpacity onPress={startVoiceRecognizing}>
+              <Image style={styles.start} source={start} />
+            </TouchableOpacity>
+          ) : (
+            <TouchableOpacity onPress={stopVoiceRecognizing}>
+              <Image style={styles.stop} source={stop} />
+            </TouchableOpacity>
+          )}
         </View>
       </ScrollView>
     </SafeAreaView>
@@ -97,22 +96,47 @@ function App(): JSX.Element {
 }
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  appHeading: {
+    fontSize: 35,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    paddingTop: 20,
+    paddingBottom: 10,
+    fontFamily: 'arial',
+    color: '#000',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  recording: {
+    color: '#898383',
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  textView: {
+    margin: 20,
+    borderColor: '#000',
+    borderWidth: 1,
+    borderRadius: 5,
+    padding: 10,
+    minHeight: 450,
   },
-  highlight: {
-    fontWeight: '700',
+  searchResult: {
+    color: '#000',
+    fontSize: 20,
+  },
+  actionButtonView: {
+    alignSelf: 'center',
+    marginTop: 50,
+  },
+  start: {
+    width: 100,
+    height: 100,
+  },
+  stop: {
+    width: 100,
+    height: 100,
+  },
+  scrollView: {
+    height: deviceHeight,
+    backgroundColor: '#fff',
   },
 });
 
 export default App;
+
